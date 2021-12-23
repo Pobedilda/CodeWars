fbdc00f6b5586af390eaaa557fce82b1001a55e4
Vyacheslav <playfog3@gmail.com>
2021-12-23 18:46:19 +0200

    Student's Final Grade

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // //Vowel remover
-const shortcut = (s) => s.replace(/[aeiou]/g,'');
+//const shortcut = (s) => s.replace(/[aeiou]/g,'');
+// //Student's Final Grade
+function finalGrade (exam, projects) {
+    if(exam > 90 || projects > 10){
+        return 100;
+    }else if(exam > 75 && projects >= 5){
+        return 90;
+    }else if(exam > 50 && projects >= 2){
+        return 75;
+    }else{
+        return 0;
+    }
+}
